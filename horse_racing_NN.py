b9a05987560bd51571d01460fd743208a61f3d5a
yusya23 <44222839+yusya23@users.noreply.github.com>
2019-02-01 16:53:06 +0900

    Add files via upload

--- horse_racing_NN.py
+++ horse_racing_NN.py
@@ -18,7 +18,7 @@ def horse_racing_NN(X_train, X_test, y_train, y_test):
                   loss='sparse_categorical_crossentropy',
                   mtrics=['accuracy'])
     
-    model.fit(X_train, y_train, epochs=100)
+    model.fit(X_train, y_train, epochs=10)
     
     pred = model.predict(X_test).argmax(axis=1)
     print((pred == y_test).sum()/ len(pred))
@@ -38,3 +38,35 @@ def horse_racing_NN2(X_train, X_test, y_train, y_test):
     
     pred = model.predict(X_test).argmax(axis=1)
     print((pred == y_test).sum()/ len(pred))
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+
+batch_size = 128
+num_class = 14
+epochs = 100
+y_train_one = keras.utils.to_categorical(y_train, num_class)
+y_test_one = keras.utils.to_categorical(y_test, num_class)
+
+model = Sequential()
+model.add(Dense(10, activation='relu', input_shape=(X_train.shape[1],)))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_class, activation='softmax'))
+
+model.summary()
+model.compile(optimizer=RMSprop(lr=0.0001),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(X_train, y_train_one,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(X_test, y_test_one))
+score = model.evaluate(X_test, y_test_one, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
